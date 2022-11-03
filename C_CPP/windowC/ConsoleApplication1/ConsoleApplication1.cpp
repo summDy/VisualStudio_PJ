9233d19b61a62902fddd8150439be7b55188dcbe
huiweiyong <307468262@qq.com>
2022-11-03 21:38:11 +0800

    CreateWindowEx 创建啦一个窗口

--- C_CPP/windowC/ConsoleApplication1/ConsoleApplication1.cpp
+++ C_CPP/windowC/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,20 +9,59 @@ extern "C" {
 #include <string.h>
 }
 
+
+
+HWND window = nullptr;
+HWND button = nullptr;
+WNDPROC defWndProc = nullptr;
+
 int main()
 {
 
-    char strTime[128];
+	char strTime[128];
 
-    std::cout << "Hello World!\n";
-    FILETIME system_time;
-    SYSTEMTIME sys_time;
-    GetSystemTimeAsFileTime(&system_time);
-    FileTimeToSystemTime(&system_time, &sys_time);
-    
-    sprintf_s(strTime, "%d-%d-%d  %d:%d:%d", sys_time.wYear, sys_time.wMonth, sys_time.wDay,sys_time.wHour, sys_time.wMinute, sys_time.wSecond);
-  
-    std::cout << strTime;
+	
+	FILETIME system_time;
+	SYSTEMTIME sys_time;
+	GetSystemTimeAsFileTime(&system_time);
+	FileTimeToSystemTime(&system_time, &sys_time);
+
+	sprintf_s(strTime,
+		"%d-%d-%d  %d:%d:%d",
+		sys_time.wYear,
+		sys_time.wMonth,
+		sys_time.wDay,
+		sys_time.wHour,
+		sys_time.wMinute,
+		sys_time.wSecond);
+
+	std::cout << strTime;
+
+	std::cout << "Hello World!\n";
+
+	
+
+
+	window = CreateWindowEx(0, WC_DIALOG, L"Form example", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 640, 480, nullptr, nullptr, nullptr, nullptr);
+	//button = CreateWindowEx(0, WC_BUTTON, L"Close", WS_CHILD | WS_VISIBLE, 10, 10, 75, 25, window, nullptr, nullptr, nullptr);
+
+	//defWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)WndProc);
+
+	ShowWindow(window, SW_SHOW);
+
+	MSG message = { 0 };
+	while (GetMessage(&message, nullptr, 0, 0))
+		DispatchMessage(&message);
+	return (int)message.wParam;
+
+
+
+	//在设备的指定位置绘制一个颜色为crColor的像素点，这样就可以画直线画长方形画心，然后就可以画漂亮的小姐姐了
+	//COLORREF SetPixel(HDC hdc, int X, int Y, COLORREF crColor)
+
+	//CreateWindow()
+
+
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
